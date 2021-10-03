4d6363ed530c69740d950b73801924d9f13676c0
Peter Kasza <peter.kasza@itinsight.hu>
2021-10-03 12:23:59 +0200

    Initial import of project files

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# socketio-intercept
-Intercept SocketIO messages as a REST api
+# SocketIO Interceptor
+
+SocketIO Interceptor is a small python script that can be used for intercepting SocketIO traffic using a regular man-in-the-middle proxy.
+
+The proxy does not need to support the decoding of SocketIO traffic or intercepting websockets as the script will translate SocketIO messages into ordinary REST api calls.
+
+Additionally the script can be used as a websocket client through the exposed REST api interface.
+
+## Installation
+
+It is best to use a virtual environment for running this script as the supported SocketIO version is dependent on the version of the python-socketio module. The requirements.txt file included with the project installs a version which is compatible with most EngineIO / SocketIO versions.
+
+See https://python-socketio.readthedocs.io/en/latest/intro.html#version-compatibility for more details on what version to install.
+
+The required modules can be installed using pip:
+
+```shell
+python3 -m pip install -r requirements.txt
+```
+
+## Running
+
+To successfully intercept traffic, you must set up your interception proxy to redirect connections to the sio-intercept listener. This can be done in many ways, however the most supported method is to replace the SocketIO connection URL in the JavaScript code fetched from the target site and to set up replacement rules to bypass the same origin policy.
+
+For example to intercept traffic from the socketio demo chat
+application, you need to:
+- Replace `var socket = io();` with `var socket = io("http://localhost:8000");` in the response body
+- Replace `Origin: https://socketio-chat-h9jt.herokuapp.com` with `Origin: http://localhost:8000` in the request headers
+- Replace `Access-Control-Allow-Origin: http://localhost:8000` with `Access-Control-Allow-Origin: https://socketio-chat-h9jt.herokuapp.com` on the response headers
+
+The first replacement rule redirects the traffic to the sio-intercept listener. The actual replacement rule may be slightly different, depending on how the application connects to the SocketIO server.
+
+The 2nd and 3rd replacement rules are used for bypassing the same origin policy. If the client is not connecting, check your browser's developer console for any errors.
+
+Next start the sio-intercept script with the appropriate options.
+
+```shell
+python sio-intercept.py --help
+Usage: sio-intercept.py [OPTIONS]
+
+Options:
+  --listen-host TEXT     [default: localhost]
+  --listen-port INTEGER  [default: 8000]
+  --mitm-proxy TEXT      [default: http://localhost:8080]
+  --sockio-url TEXT      [default: https://socketio-chat-h9jt.herokuapp.com]
+  --help
+```
+
+If everything is set up correctly, you should see the SocketIO messages in your interception proxy:
+
+![Example interception proxy session](example.png)
+
+## Options
+
+The `listen-host` and `listen-port` are used to set the listening address for the SocketIO and RestAPI servers. Generally you can leave the default setting.
+
+The `mitm-proxy` setting is used to specify the listening host/port for your interception proxy.
+
+The `sockio-url` parameter must be set to the correct SocketIO url for the target application.
+
+Proxy settings for posting intercepted traffic (`/emit` and `/poll`) requests) will be loaded from the `http_proxy` and `https_proxy` environment variables.
+
+## REST api interface
+
+The script implements a simple REST api interface, with the following methods:
+
+- GET `/connect`: Connect to the specified SocketIO endpoint and return a session identifier
+
+- GET `/disconnect/<SID>`: Disconnect from the session identified by SID
+
+- POST `/emit/<SID>`: Send the SocketIO event specified in the POST body to the session identified by SID. The POST body must contain a JSON object with the following fields: `{"event": "event-name-to-emit", "args": ["event-args-to-emit"]}`
+
+- GET `/poll/<SID>`: Poll for new events received for the session identified by SID
---
+++ sio-intercept.py
@@ -0,0 +1,172 @@
+from asgiref.wsgi import WsgiToAsgi
+from flask import Flask, request
+from dataclasses import dataclass
+
+import aiohttp
+import typing
+import socketio
+import uuid
+import json
+import asyncio
+import uvicorn
+import click
+import httpx
+
+
+@dataclass
+class Session:
+    client: socketio.AsyncClient
+    lock: asyncio.Lock
+    events: [typing.List[typing.Any]]
+
+
+class ServerCatchAllNamespace(socketio.AsyncNamespace):
+    def __init__(self, namespace, listener, proxies={}, poll_wait=1):
+        socketio.AsyncNamespace.__init__(self, namespace)
+
+        # url for the uvicorn server listener
+        self.listener = listener
+
+        # maps from socket io sessions to rest sessions
+        self.sessions = {}
+
+        # interception proxies to use
+        self.proxies = proxies
+
+        # time to sleep between polls
+        self.poll_wait = poll_wait
+
+    async def trigger_event(self, *args):
+        async def poll(sid):
+            async with httpx.AsyncClient(proxies=self.proxies) as client:
+                while sid in self.sessions:
+                    response = await client.get('{}/poll/{}'.format(
+                        self.listener, self.sessions[sid]
+                    ))
+                    events = response.json()
+                    for event in events:
+                        method, args = event[0], event[1:]
+                        await self.emit(method, *args, room=sid)
+                        await asyncio.sleep(0)
+                    await asyncio.sleep(self.poll_wait)
+
+        method, sid, args = args[0], args[1], args[2:]
+
+        async with httpx.AsyncClient(proxies=self.proxies) as client:
+            if method == 'connect':
+                response = await client.get('{}/connect'.format(self.listener))
+                self.sessions[sid] = response.text
+                asyncio.create_task(poll(sid))
+            elif method == 'disconnect':
+                await client.get(
+                    '{}/disconnect/{}'.format(
+                        self.listener, self.sessions[sid])
+                )
+                del self.sessions[sid]
+            else:
+                await client.post(
+                    '{}/emit/{}'.format(
+                        self.listener, self.sessions[sid]),
+                    json={'event': method, 'args': args}
+                )
+
+
+class ClientCatchAllNamespace(socketio.AsyncClientNamespace):
+    def __init__(self, namespace, session=None):
+        socketio.AsyncClientNamespace.__init__(self, namespace)
+        self.session = session
+
+    async def trigger_event(self, *args):
+        async with self.session.lock:
+            self.session.events.append(args)
+
+
+class RestAPIServer(Flask):
+    def __init__(self, sockio_url):
+        Flask.__init__(self, __name__)
+        self.sockio_url = sockio_url
+        self.sessions = {}
+        self._init_rules()
+
+    def _init_rules(self):
+        self.add_url_rule("/connect", "connect", self.connect)
+        self.add_url_rule("/disconnect/<sid>", "disconnect", self.disconnect)
+        self.add_url_rule("/emit/<sid>", "emit", self.emit, methods=['POST'])
+        self.add_url_rule("/poll/<sid>", "poll", self.poll)
+
+    # @app.route("/connect")
+    async def connect(self):
+        sid = str(uuid.uuid4())
+
+        # get http_proxy and https_proxy from environment
+        http_session = aiohttp.ClientSession(trust_env=True)
+        session = Session(
+            socketio.AsyncClient(logger=False, engineio_logger=False,
+                                 http_session=http_session),
+            asyncio.Lock(),
+            []
+        )
+        self.sessions[sid] = session
+
+        session.client.register_namespace(
+            ClientCatchAllNamespace('/', session=session))
+
+        await session.client.connect(self.sockio_url)
+        return sid
+
+    # @app.route("/disconnect/<sid>")
+    async def disconnect(self, sid):
+        if sid in self.sessions:
+            session = self.sessions[sid]
+            await session.client.disconnect()
+            del self.sessions[sid]
+            return 'OK'
+        return 'KO'
+
+    # @app.route("/emit/<sid>", methods=['POST'])
+    async def emit(self, sid):
+        if sid in self.sessions:
+            session = self.sessions[sid]
+            data = request.json
+            if data is not None and 'event' in data and 'args' in data:
+                await session.client.emit(data['event'], *data['args'])
+                return 'OK'
+        return 'KO'
+
+    # @app.route("/poll/<sid>")
+    async def poll(self, sid):
+        if sid in self.sessions:
+            session = self.sessions[sid]
+            async with session.lock:
+                events = json.dumps(session.events)
+                session.events.clear()
+                return events
+        return 'KO'
+
+
+@click.command()
+@click.option('--listen-host', default='localhost', type=str, show_default=True)
+@click.option('--listen-port', default=8000, type=int, show_default=True)
+@click.option('--mitm-proxy', default='http://localhost:8080', type=str, show_default=True)
+@click.option('--sockio-url', default='https://socketio-chat-h9jt.herokuapp.com', type=str, show_default=True)
+def main(listen_host, listen_port, mitm_proxy, sockio_url):
+    proxies = {
+        'http://': mitm_proxy,
+        'https://': mitm_proxy
+    }
+
+    sock_srv = socketio.AsyncServer(
+        async_mode='asgi', logger=False, engineio_logger=False)
+
+    sock_srv.register_namespace(
+        ServerCatchAllNamespace('/', 'http://{}:{}'.format(
+            listen_host, listen_port), proxies)
+    )
+
+    rest_app = WsgiToAsgi(RestAPIServer(sockio_url))
+    main_app = socketio.ASGIApp(sock_srv, other_asgi_app=rest_app)
+
+    uvicorn.run(main_app, host=listen_host, port=listen_port)
+
+if __name__ == '__main__':
+    main()
